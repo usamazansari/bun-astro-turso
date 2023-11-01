3f29d81aed7926588f3dfc0f007d10f77c9f2ee6
Usama Ansari <usamazansari@outlook.com>
2023-11-01 20:28:08 +0100

    feat: add markdoc to render blog content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import markdoc from '@astrojs/markdoc';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), tailwind()],
+  integrations: [mdx(), react(), tailwind(), markdoc()],
 });
