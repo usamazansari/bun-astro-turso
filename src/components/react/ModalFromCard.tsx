14b0108e91b0aede84bed1fd6aa318cb0abedcdd
Usama Ansari <usamazansari@outlook.com>
2023-11-01 12:30:57 +0100

    chore: remove modal component

--- src/components/react/ModalFromCard.tsx
+++
@@ -1,9 +0,0 @@
-import { Blockquote } from "@mantine/core";
-
-export function ModalFromCard() {
-  return (
-    <Blockquote color="blue" cite="Forrest Gump" className="mt-4">
-      Life is like an npm install - you never know what you are going to get.
-    </Blockquote>
-  );
-}
